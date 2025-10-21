db0c8411613d9f84374e31409528aaa9fb280634
Liucong-JunZi <lc20040517@gmail.com>
2025-10-21 15:30:43 +0800

    添加循环速度计算的串行和并行实现，包含loop_speed和loop_speed_parallel文件

---
+++ 2_class/loop_speed.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+int main(){
+    int n=200000000;
+    std::vector<int> data(n);
+    for(int i= 0;i<n;i++){
+        data[i]=i;
+
+    }
+    long long sum   = 0;
+    auto start_time =std::chrono::high_resolution_clock::now();
+    for (int i =0 ;i<n;i++){
+        sum += (long long)data[i]*data[i];
+
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed =end_time - start_time;
+    std::cout<<"out put is:"<<sum<<std::endl;
+    std::cout<<"time cost is "<<elapsed.count()<<" s"<<std::endl;
+    return 0;
+}
---
+++ 2_class/loop_speed_parallel.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <chrono>
+
+int main(){
+    int n=200000000;
+    std::vector<int> data(n);
+    for(int i= 0;i<n;i++){
+        data[i]=i;
+
+    }
+    long long sum   = 1;
+    auto start_time =std::chrono::high_resolution_clock::now();
+    #pragma omp parallel for reduction(*:sum)
+    for (int i =0 ;i<n;i++){
+        sum += (long long)data[i]*data[i];
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed =end_time - start_time;
+    std::cout<<"out put is:"<<sum<<std::endl;
+    std::cout<<"time cost is "<<elapsed.count()<<" s"<<std::endl;
+    return 0;
+}
